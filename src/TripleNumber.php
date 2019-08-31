b1e9abf94c42d5dd1eefca80283924050590cc55
Kenji Suzuki <kenji.uui@gmail.com>
2019-08-31 19:36:06 +0900

    refactor: remove the same code
    the if statements are the same conditions as `$binary->isTripleNumber()`

--- src/TripleNumber.php
+++ src/TripleNumber.php
@@ -15,35 +15,18 @@ class TripleNumber
         while ($binary->isTripleNumber()) {
             $binary_string = '0' . $binary->string();
 
-            if (strpos($binary_string, '000') !== false) {
-                preg_match('/(.*?)(1000|0111)(.*)/', $binary_string, $matches);
+            preg_match('/(.*?)(1000|0111)(.*)/', $binary_string, $matches);
 
-                $before = $matches[1];
-                $after = $matches[3];
+            $before = $matches[1];
+            $after = $matches[3];
 
-                $after = substr(
-                    str_repeat('001', (int) ceil(strlen($after) / 3)),
-                    0,
-                    strlen($after)
-                );
-                $binary_string = $before . '1001' . $after;
-                $binary = new Binary($binary_string);
-            }
-
-            if (strpos($binary_string, '111') !== false) {
-                preg_match('/(.*?)(1000|0111)(.*)/', $binary_string, $matches);
-
-                $before = $matches[1];
-                $after = $matches[3];
-
-                $after = substr(
-                    str_repeat('001', (int) ceil(strlen($after) / 3)),
-                    0,
-                    strlen($after)
-                );
-                $binary_string = $before . '1001' . $after;
-                $binary = new Binary($binary_string);
-            }
+            $after = substr(
+                str_repeat('001', (int) ceil(strlen($after) / 3)),
+                0,
+                strlen($after)
+            );
+            $binary_string = $before . '1001' . $after;
+            $binary = new Binary($binary_string);
         }
 
         return $binary;
